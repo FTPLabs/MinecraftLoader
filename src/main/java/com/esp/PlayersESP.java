a603b8416363cff74e993f47bb7786379176924d
FTPLab <destrooyy@protonmail.com>
2026-05-22 12:11:41 +0300

    fix: revert to no-arg constructor — Forge 52.1.x requires <init>() not <init>(IEventBus)

--- src/main/java/com/esp/PlayersESP.java
+++ src/main/java/com/esp/PlayersESP.java
@@ -4,6 +4,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,8 +14,8 @@ public class PlayersESP {
     public static final String MOD_ID = "playersesp";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-    // Forge 1.21.1: inject IEventBus directly instead of FMLJavaModLoadingContext.get()
-    public PlayersESP(IEventBus modEventBus) {
+    public PlayersESP() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::clientSetup);
     }
 
