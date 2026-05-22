8fd6f3657c066c8cdbe65cd916eb7f3cfa002921
FTPLab <destrooyy@protonmail.com>
2026-05-22 13:55:01 +0300

    feat: EspKeyTickHandler Forge bus tick

---
+++ src/main/java/com/esp/EspKeyTickHandler.java
@@ -0,0 +1,29 @@
+package com.esp;
+
+  import net.minecraft.client.Minecraft;
+  import net.minecraftforge.api.distmarker.Dist;
+  import net.minecraftforge.event.TickEvent;
+  import net.minecraftforge.eventbus.api.SubscribeEvent;
+  import net.minecraftforge.fml.common.Mod;
+
+  @Mod.EventBusSubscriber(modid = PlayersESP.MOD_ID, value = Dist.CLIENT)
+  public class EspKeyTickHandler {
+      @SubscribeEvent
+      public static void onTick(TickEvent.ClientTickEvent event) {
+          if (event.phase != TickEvent.Phase.END) return;
+          Minecraft mc = Minecraft.getInstance();
+          if (mc.level == null) return;
+
+          while (EspKeyHandler.KEY_TOGGLE.consumeClick()) {
+              EspConfig.espEnabled = !EspConfig.espEnabled;
+              EspConfig.save();
+          }
+          while (EspKeyHandler.KEY_GUI.consumeClick()) {
+              if (mc.screen == null) mc.setScreen(new EspScreen());
+          }
+          while (EspKeyHandler.KEY_ORE.consumeClick()) {
+              EspConfig.oreEsp = !EspConfig.oreEsp;
+              EspConfig.save();
+          }
+      }
+  }
